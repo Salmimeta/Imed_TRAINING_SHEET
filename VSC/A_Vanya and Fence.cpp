c75f041ec507d511e24de5ae32b9aa2a7c924a69
Salma_Hamrit <120305246+Salmimeta@users.noreply.github.com>
2023-08-31 16:30:25 +0100

    ProblemSolving

---
+++ VSC/A_Vanya and Fence.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+ 
+ 
+int main()
+{
+    using namespace std;
+    int n,h,i,w = 0;   cin >> n >> h;
+    int t[n];
+ 
+    for (i = 0  ;  i < n  ; i++)
+        cin >> t[i];
+    for (i = 0  ;  i < n  ;  i++)
+        (t[i] > h)?w+=2:w++;
+ 
+    cout << w;
+    return 0;
+}
